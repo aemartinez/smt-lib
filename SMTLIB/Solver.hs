cb1be2a9302cee794d4ee922d5d2dca9c9eb9751
Agustin Eloy Martinez Sune <agusms@amazon.com>
2021-10-12 01:12:51 -0400

    binding with z3

--- SMTLIB/Solver.hs
+++ SMTLIB/Solver.hs
@@ -5,6 +5,18 @@ module SMTLIB.Solver
 where
 
 import SMTLIB
+import System.IO
+import System.Process
+import Data.String.Utils
+import Control.Monad
 
-isSat :: Script -> Bool
-isSat script = False
+isSat :: Script -> IO Bool
+isSat script = do
+    let tmpFile = "./tmp.smt2"
+    writeFile tmpFile (show script)
+    (_, Just hout, _, _) <-
+      createProcess (proc "z3" ["-smt2", tmpFile]){ std_out = CreatePipe }
+    sOut <- hGetContents hout
+    s <- return (strip sOut)
+    return ("sat" == s)
+    
